3faae227d6b5714796f76b126520e1a1c7da30ac
Piotr Wójcik <piotr.wojcik@rivierasolutions.pl>
2015-09-01 00:02:49 +0200

    Added asp.net version.

---
+++ aspnet/SSQSignonAuthProxy/AuthProxyController.cs
@@ -0,0 +1,81 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SSQSignon
+{
+    public abstract class AuthProxyController : ApiController
+    {
+        public class AuthRequestModel
+        {
+            public string grant_type { get; set; }
+
+            public string client_id { get; set; }
+
+            public string code { get; set; }
+
+            public string redirect_uri { get; set; }
+
+            public string username { get; set; }
+
+            public string password { get; set; }
+
+            public string refresh_token { get; set; }
+
+            public string scope { get; set; }
+
+            public string client_secret { get; set; }
+        }
+
+        public AuthProxyController(string moduleName, string clientId, string clientSecret)
+        {
+            ModuleName = moduleName;
+            ClientId = clientId;
+            ClientSecret = clientSecret;
+            restClient = new RestClient(string.Format("https://tinyusers.azurewebsites.net/{0}/auth", moduleName));
+            if (!string.IsNullOrEmpty(clientSecret))
+            {
+                restClient.Authenticator = new RestSharp.Authenticators.HttpBasicAuthenticator(clientId, clientSecret);
+            }
+        }
+
+        public virtual dynamic Post(AuthRequestModel model)
+        {
+            var request = new RestRequest(Method.POST);
+            request.AddJsonBody(model);
+
+            var response = restClient.Execute<AuthorizationResponse>(request);
+            if (response.ErrorException == null && response.StatusCode.Equals(HttpStatusCode.OK))
+            {
+                return Json(response.Data);
+            }
+            else
+            {
+                return StatusCode(HttpStatusCode.BadGateway);
+            }
+        }
+
+        protected string ModuleName { get; set; }
+
+        protected string ClientId { get; set; }
+
+        protected string ClientSecret { get; set; }
+
+        private class AuthorizationResponse
+        {
+            public string user_id { get; set; }
+
+            public string scope { get; set; }
+
+            public string access_token { get; set; }
+
+            public string refresh_token { get; set; }
+        }
+
+        private RestClient restClient;
+    }
+}
---
+++ aspnet/SSQSignonAuthProxy/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SSQSignon.AuthProxy")]
+[assembly: AssemblyDescription("An abstract asp.net web api controller for poxying requests  to the ssqsignon.com /auth endpoint.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Riviera Solutions")]
+[assembly: AssemblyProduct("SSQSignonAuthProxy")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c37caa81-e0e5-42dc-99d0-6800dadb5b9e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
